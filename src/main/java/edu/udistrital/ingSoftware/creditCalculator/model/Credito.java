343c4769b2719c3ce35a2e4be3a5d165cfc6182d
fabianmartinez <fabianemartinez@gmail.com>
2018-05-23 19:35:16 -0500

    update

--- src/main/java/edu/udistrital/ingSoftware/creditCalculator/model/Credito.java
+++ src/main/java/edu/udistrital/ingSoftware/creditCalculator/model/Credito.java
@@ -17,7 +17,39 @@ public class Credito {
     private ResponseCredito responseCredito;
 
     public ResponseCredito CalcularFijo(){
-        //TODO: Calcular credito variable
+
+        responseCredito = new ResponseCredito();
+
+        double saldo = monto;
+        int numCuota = 1;
+        double tasaInteresMes = Math.pow(1 + (tasaInteres/100.0),1.0/12.0)-1;
+
+        if(plazo == 0)
+        {
+
+            while (saldo > 0){
+                double interes = Math.round(saldo*tasaInteresMes*100.0)/100.0;
+                double abono = (saldo > cuota)?cuota - interes:saldo;
+                abono = Math.round(abono*100.0)/100.0;
+                responseCredito.AddCuota(Math.round(saldo - abono),abono,interes,abono + interes,numCuota);
+                numCuota++;
+                saldo = saldo - abono;
+            }
+
+        }else if (cuota == 0){
+
+            cuota = monto * (tasaInteresMes / (1 - Math.pow(1 + tasaInteresMes, -1*plazo)));
+            cuota = Math.round(cuota*100.0)/100.0;
+
+            for (int i = 0 ; i < plazo ; i++){
+                double interes = Math.round(saldo*tasaInteresMes*100.0)/100.0;
+                double abono = cuota - interes;
+                responseCredito.AddCuota(Math.round(saldo - abono),Math.round(abono),interes,cuota,i+1);
+                saldo = saldo - abono;
+                saldo = Math.round(saldo);
+            }
+
+        }
         return responseCredito;
     }
 
@@ -25,21 +57,29 @@ public class Credito {
 
         responseCredito = new ResponseCredito();
 
+        double saldo = monto;
+        int numCuota = 1;
+        double tasaInteresMes = Math.pow(1 + (tasaInteres/100.0),1.0/12.0)-1;
+
         if(plazo == 0)
         {
-            double saldo = monto;
-            int numCuota = 1;
-            double tasaInteresMes = (1+Math.pow(tasaInteres/100.0,1.0/((int)(monto/cuota))))-1;
-            tasaInteresMes = Math.round(tasaInteresMes*100)/100.0;
 
             while (saldo > 0){
-                double interes = Math.round(saldo*tasaInteresMes*100)/100.0;
+                double interes = Math.round(saldo*tasaInteresMes*100.0)/100.0;
                 responseCredito.AddCuota(saldo-cuota,cuota,interes,cuota+interes,numCuota);
                 numCuota++;
                 saldo = saldo - cuota;
             }
 
         }else if (cuota == 0){
+
+            cuota = monto/(double) plazo;
+
+            for (int i = 0 ; i < plazo ; i++){
+                double interes = Math.round(saldo*tasaInteresMes*100.0)/100.0;
+                responseCredito.AddCuota(saldo-cuota,cuota,interes,cuota+interes,i+1);
+                saldo = saldo - cuota;
+            }
 
         }
         return responseCredito;
